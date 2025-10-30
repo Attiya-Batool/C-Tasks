05c1ac0fbc944821ebff32827c9bd9ac433b0e8e
Attiya-Batool <attiyabatool731@gmail.com>
2025-10-29 23:39:43 -0700

    Add files via upload

---
+++ Parameterized Constructor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class student{
+private:
+	int a,b;
+	public:
+	student(int x, int y){
+		a=x, b=y;
+	}
+	void show(){
+	cout<<a<<b;
+	}
+};
+int main(){
+student s(7,8);
+student s1(5,7);
+s.show();
+s1.show();
+}
