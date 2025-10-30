9c2d53108ac0463f2d9bc2a236b61614659d3421
Attiya-Batool <attiyabatool731@gmail.com>
2025-10-29 21:04:07 -0700

    Add files via upload

---
+++ Diamond shape pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n=5;
+	for(int i=1; i<=5; i++)
+	{
+		for(int space=1; space<=n-i; space++){
+		cout<<" ";
+		}
+		for(int j=1; j<=i*2-1; j++){
+		cout<<"*";
+		}
+		cout<<endl;
+	}
+for(int i=5; i>=1; i--){
+	for(int space=1; space<=n-i; space++){
+	cout<<" ";
+	}
+	for(int j=1; j<=i*2-1; j++){
+		cout<<"*";
+	}
+	cout<<endl;
+}
+}
