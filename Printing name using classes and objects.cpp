17be56e8d25b07c0881a01eaef3c8212aaec84a8
Attiya-Batool <attiyabatool731@gmail.com>
2025-10-29 23:55:22 -0700

    Add files via upload

---
+++ Printing name using classes and objects.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Name
+{
+	public:
+		void display()
+		{
+			cout<<"Attiya Batool!";
+		}
+};
+int main()
+{
+	Name obj;
+	obj.display();
+	getch();
+}
